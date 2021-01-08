1606f6ffeefb0bf91023db46245afbbdfca20d12
Sandro0330 <sandro030486@gmail.com>
2021-01-07 22:54:09 -0300

    Service layer, component registration

---
+++ src/main/java/com/maven/curso/services/UserService.java
@@ -0,0 +1,27 @@
+package com.maven.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maven.curso.entities.User;
+import com.maven.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired // faz a injeção de dependência
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);		
+		return obj.get();
+	}
+
+}
