72255ef38645c28feb901c9b6a067135500bab7b
Sandro0330 <sandro030486@gmail.com>
2021-01-12 16:53:46 -0300

    Payment, one-to-one association

--- src/main/java/com/maven/curso/config/TestConfig.java
+++ src/main/java/com/maven/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.maven.curso.entities.Category;
 import com.maven.curso.entities.Order;
 import com.maven.curso.entities.OrderItem;
+import com.maven.curso.entities.Payment;
 import com.maven.curso.entities.Product;
 import com.maven.curso.entities.User;
 import com.maven.curso.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1); //associando pedido com pagamento
+		
+		orderRepository.save(o1);
 	}	
 }
